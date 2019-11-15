99cb31f734f8b4b75c93724a0a567d9ec63f69d2
Kyle Gonzales <kylep.gonz@gmail.com>
2019-11-14 21:08:58 -0500

    updated sprint reports

--- templates.go
+++ templates.go
@@ -10,7 +10,9 @@ func init() {
 	m := template.Must
 	p := template.ParseFiles
 	tmpl["index"] = m(p("templates/index.gohtml", "templates/layout.gohtml"))
-	tmpl["layout"] = m(p("templates/aboutindex.gohtml", "templates/layout.gohtml"))
-	tmpl["events"] = m(p("templates/eventsindex.gohtml", "templates/layout.gohtml"))
+	tmpl["report1"] = m(p("templates/SoftwareDevelopment_Sprint1.gohtml", "templates/layout.gohtml"))
+	tmpl["report2"] = m(p("templates/SoftwareDevelopment_Sprint2.gohtml", "templates/layout.gohtml"))
+	tmpl["report3"] = m(p("templates/SoftwareDevelopment_Sprint3.gohtml", "templates/layout.gohtml"))
+	//tmpl["events"] = m(p("templates/eventsindex.gohtml", "templates/layout.gohtml"))
 	tmpl["about"] = m(p("templates/aboutpage.gohtml", "templates/layout.gohtml"))
 }
