a5c6b13d44b7f15be00a0bcbf8d5dacdc3623cc9
Kyle Gonzales <kylep.gonz@gmail.com>
2019-12-08 19:32:41 -0500

    added improvements to test code

--- api_controllers.go
+++ api_controllers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"time"
 	//"io/ioutil"
 	"github.com/go-chi/chi"
 )
@@ -24,7 +25,14 @@ func apiEventListController(w http.ResponseWriter, r *http.Request) {
 
 func apiEventController(w http.ResponseWriter, r *http.Request) {
 	type eventListType struct {
-		Events Event `json:"events"`
+		ID          int       `json:"id"`
+		Title       string    `json:"title"`
+		Location    string    `json:"location"`
+		Image       string    `json:"image"`
+		Date        time.Time `json:"date"`
+		Description string    `json:"description"`
+		Category    string    `json:"category"`
+		Attending   []string  `json:"attending"`
 	}
 	eventIDstring := chi.URLParam(r, "eventID")
 	eventID, err := strconv.Atoi(eventIDstring)
@@ -39,7 +47,16 @@ func apiEventController(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	responseData := eventListType{Events: event}
+	responseData := eventListType{
+		ID:          event.ID,
+		Title:       event.Title,
+		Location:    event.Location,
+		Image:       event.Image,
+		Date:        event.Date,
+		Description: event.Description,
+		Category:    event.Category,
+		Attending:   event.Attending,
+	}
 	jsonResponseData, err := json.MarshalIndent(responseData, "", " ")
 	if err != nil {
 		http.Error(w, "Could not serialize json data!", http.StatusInternalServerError)
